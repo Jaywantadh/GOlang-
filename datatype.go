f9ffdd9dbb0790a85b661f8793a9a230f60a9de6
Jaywant Adhau <119965656+Jaywantadh@users.noreply.github.com>
2024-07-23 09:21:18 +0530

    Add files via upload

---
+++ datatype.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	var a int = 10
+	var b bool = true
+	var c float32 = 3.14
+	var d float64 = 3.1448573495853495
+	var e string = "This is string"
+
+	fmt.Println("Integer: ", a)
+	fmt.Println("Boolean: ", b)
+	fmt.Println("Float 32bit: ", c)
+	fmt.Println("Float 64bit: ", d)
+	fmt.Println("String: ", e)
+}
